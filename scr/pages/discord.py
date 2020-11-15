9c9254311a0808be3a29282bb573b8592c58e664
Gabriel Bellon <gabriel.bellon@contaazul.com>
2020-11-14 23:27:59 -0300

    typo

--- scr/pages/discord.py
+++ scr/pages/discord.py
@@ -21,8 +21,8 @@ def write():
     year = round(days / 365, 1)
 
     st.markdown(f"## - No Discord tivemos um total de **{(messages)}** enviadas ")
-    st.markdown(f"## - Além disso, nas messas de bar e canais de audios tivemos **{(minutes)} minutos** de conversas ")
-    st.markdown(f"## - Com um total de  **{hours} horas** de visualizações, o que dá **{days} dias** consecutivo ouvindo tudo !!")
+    st.markdown(f"## - Além disso, nas messas de bar e canais de áudios tivemos **{(minutes)} minutos** de conversas ")
+    st.markdown(f"## - Com um total de  **{hours} horas** de áudios, o que dá **{days} dias** consecutivo ouvindo tudo !!")
 
     plot_discord(discord_text_df,discord_voice_df)
 
