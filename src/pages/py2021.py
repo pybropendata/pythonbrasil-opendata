7647336c624c036f3e7d5b6ffbd0c7b06c86a96c
Gabriel Bellon <gabubellon@gmail.com>
2021-10-21 00:44:10 -0300

    home page

---
+++ src/pages/py2021.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import util
+
+has_ok=False
+def write():
+    util.write_title("- PyBR2021 - ")
+    util.render_img("./assets/pybr2021-logo-abreviado.png", "2021")
+
+    st.markdown("---")
+    st.markdown(" **EM DESENVOLVIMENTO**")
+    st.markdown("---")
